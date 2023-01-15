57c9cf52028d92bb7222eef583fb6a9258aca382
Aris-Arsen Kashari <51513565+arisyo13@users.noreply.github.com>
2023-01-16 00:42:28 +0100

    chore: lint fixes

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -14,13 +14,13 @@ export async function getStaticPaths() {
     paths,
     fallback: false, // can also be true or 'blocking'
   };
-};
+}
 
 export async function getStaticProps(context: { params: { slug: SlugType }}): Promise<{ props: { project: Project } }> {
   const project: Project | undefined = projects.find(item => item.slug === context.params?.slug);
   const error: Project = { name: 'error', image: undefined, slug: context.params?.slug };
 
   return { props: { project: project || error } };
-};
+}
 
-export default SinglePost
+export default SinglePost;
