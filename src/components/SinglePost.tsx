aa00ea1934a91ee26d4e1a3b91de71859b1707bb
Aris-Arsen Kashari <51513565+arisyo13@users.noreply.github.com>
2023-01-15 23:45:06 +0100

    fixed server props issue

---
+++ src/components/SinglePost.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import Container from './Container';
+import FireAlarm from './FireAlarm';
+
+export type SlugType = 'fire' | 'cinema' | 'pizza';
+
+export interface Project {
+  name: string;
+  image?: string;
+  slug: SlugType;
+}
+
+interface ProjectProps {
+    project: Project
+  }
+
+const SinglePost: FC<ProjectProps> = ({ project }) => {
+    if (!project){
+      return null;
+    }
+  
+    switch (project.slug) {
+      case 'fire':
+        return <Container><FireAlarm /></Container>;
+      case 'cinema':
+        return <Container><h1>Cinema</h1></Container>;
+      case 'pizza':
+        return <Container><h1>Pizza Napoli test</h1></Container>;
+      default:
+        break;
+    }
+  
+    const { name } = project;
+  
+    return (
+      <div className="p-20">{name}</div>
+    );
+  };
+
+
+
+export default SinglePost;
--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,52 +1,12 @@
-import React, { FC } from 'react';
+import { Project, SlugType } from '../../components/SinglePost';
 
-import Container from '../../components/Container';
-import FireAlarm from '../../components/FireAlarm';
-
-type SlugType = 'fire' | 'cinema' | 'pizza';
-
-interface Project {
-  name: string;
-  image: string;
-  slug: SlugType;
-}
-
-interface ProjectProps {
-  project: Project
-}
-
-const SinglePost: FC<ProjectProps> = ({ project }) => {
-  if (!project){
-    return null;
-  }
-
-  switch (project.slug) {
-    case 'fire':
-      return <Container><FireAlarm /></Container>;
-    case 'cinema':
-      return <Container><h1>Cinema</h1></Container>;
-    case 'pizza':
-      return <Container><h1>Pizza Napoli test</h1></Container>;
-    default:
-      break;
-  }
-
-  const { name } = project;
-
-  return (
-    <div className="p-20">{name}</div>
-  );
-};
-
-export async function getServerSideProps(context: { params: { slug: SlugType }}): Promise<{ props: ProjectProps }> {
+export async function getServerSideProps(context: { params: { slug: SlugType }}): Promise<{ props: { project: Project } }> {
   const projects: Project[] = [
     { name: 'Alaaarm', image: '/assets/arrow.png', slug: 'fire' },
     { name: 'Fire', image: '/assets/arrow.png', slug: 'fire' }
   ];
   const project: Project | undefined = projects.find(item => item.slug === context.params?.slug);
-  const error = { name: 'error', slug: context.params?.slug, image: 'd' };
+  const error: Project = { name: 'error', slug: context.params?.slug, image: undefined };
 
   return { props: { project: project || error } };
 }
-
-export default SinglePost;
