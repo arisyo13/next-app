fa611135b023c0b4d6625e9d8aa7b27aa941b1a9
Aris-Arsen Kashari <51513565+arisyo13@users.noreply.github.com>
2023-01-16 00:40:33 +0100

    chore: fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: undefined
 }
 
 module.exports = nextConfig
--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -8,18 +8,19 @@ const projects: Project[] = [
 export async function getStaticPaths() {
   const paths = projects.map((post) => ({
     params: { slug: post.slug! },
-  }))
+  }));
+
   return {
     paths,
     fallback: false, // can also be true or 'blocking'
-  }
-}
+  };
+};
 
 export async function getStaticProps(context: { params: { slug: SlugType }}): Promise<{ props: { project: Project } }> {
   const project: Project | undefined = projects.find(item => item.slug === context.params?.slug);
   const error: Project = { name: 'error', image: undefined, slug: context.params?.slug };
 
   return { props: { project: project || error } };
-}
+};
 
 export default SinglePost
